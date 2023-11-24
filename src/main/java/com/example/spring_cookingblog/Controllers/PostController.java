d1b2834a96baa41b2598d5832408aba0630ce0e0
r4yz7 <mmischenko850@gmail.com>
2023-11-24 19:58:28 +0200

    com

--- src/main/java/com/example/spring_cookingblog/Controllers/PostController.java
+++ src/main/java/com/example/spring_cookingblog/Controllers/PostController.java
@@ -1,10 +1,13 @@
 package com.example.spring_cookingblog.Controllers;
 
+import com.example.spring_cookingblog.Models.Comment;
 import com.example.spring_cookingblog.Models.Post;
 import com.example.spring_cookingblog.Models.User;
+import com.example.spring_cookingblog.Repositories.CommentRepository;
 import com.example.spring_cookingblog.Repositories.PostRepository;
 import com.example.spring_cookingblog.Repositories.UserRepository;
 import jakarta.servlet.http.HttpSession;
+import jakarta.websocket.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
@@ -18,6 +21,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 @Controller
 public class PostController {
@@ -25,11 +29,14 @@ public class PostController {
     PostRepository postRepository;
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    CommentRepository commentRepository;
     @GetMapping("/")
-    public String getPosts(Model model){
+    public String getPosts(Model model,HttpSession session){
         Iterable< Post>posts = postRepository.findAll();
         model.addAttribute("posts",posts);
-
+        model.addAttribute("session",session);
         return "post";
     }
     @GetMapping("/post/new")
@@ -46,7 +53,6 @@ public class PostController {
         Post post = new Post();
         post.setContext(context);
         post.setTitle(title);
-        post.setAvgRating(0);
         Long userId = (Long) session.getAttribute("userId");
         User user = userRepository.findById(userId).orElse(null);
         if(user!=null)
@@ -72,8 +78,121 @@ public class PostController {
         Post post = postRepository.findById(id).orElse(null);
         if(post!=null){
             model.addAttribute("post",post);
+
             return "details";
         }
         return "accessDenied";
     }
+
+    @GetMapping("/post/{id}/comment")
+    public String sendComment(@PathVariable(value = "id")Long id,HttpSession session, Model model){
+        Long userId = (long) session.getAttribute("userId");
+        if(userId!=null){
+            Post post = postRepository.findById(id).orElse(null);
+            model.addAttribute("post", post);
+            return "sendComment";
+        }
+        return "redirect:/user/login";
+    }
+    @PostMapping("/post/{id}/comment")
+    public String sendComment(@PathVariable(value = "id")Long id,@RequestParam String text, @RequestParam int rating, HttpSession session, Model model){
+        Post post = postRepository.findById(id).orElse(null);
+        Comment comment = new Comment();
+        comment.setText(text);
+        comment.setRating(rating);
+        Long userId = (Long) session.getAttribute("userId");
+        User user = userRepository.findById(userId).orElse(null);
+        if(user!=null)
+            comment.setUser(user);
+        if(post!=null)
+        {
+            comment.setPost(post);
+            calcRating(post,rating);
+            postRepository.save(post);
+        }
+        commentRepository.save(comment);
+        return "redirect:/";
+    }
+
+    private void calcRating(Post post, int rating) {
+        List<Comment> comments = post.getComments();
+        if (comments != null && !comments.isEmpty()) {
+            int totalRating = 0;
+            for (Comment comment : comments) {
+                totalRating += comment.getRating();
+            }
+            int avgRating = (totalRating + rating) / (comments.size() + 1);
+            post.setAvgRating(avgRating);
+        } else {
+            post.setAvgRating(rating);
+        }
+    }
+
+    @GetMapping("/post/{id}/edit")
+    private String postEdit(@PathVariable(value = "id")Long id, Model model, HttpSession session){
+        String userRole = (String) session.getAttribute("userRole");
+        Long userId = (Long) session.getAttribute("userId");
+        Post post = postRepository.findById(id).orElse(null);
+        if(userRole=="radmin" || userId == post.getUser().getId()){
+            if(post!=null){
+                model.addAttribute("post",post);
+                return "postEdit";
+            }
+            return "redirect:/";
+        }
+        return "accessDenied";
+    }
+
+    @GetMapping("/post/{id}/delete")
+    private String postDelete(@PathVariable(value = "id") Long id, Model model,HttpSession session){
+        String userRole = (String) session.getAttribute("userRole");
+        Long userId = (Long) session.getAttribute("userId");
+        Post post = postRepository.findById(id).orElse(null);
+        if(userRole=="radmin" || userId == post.getUser().getId()){
+            if(post!=null){
+                postRepository.delete(post);
+            }
+            return "redirect:/";
+        }
+        return "accessDenied";
+    }
+
+    @PostMapping("/postSearch")
+    private String postSearch(@RequestParam String title, Model model){
+    List<Post> postsSearch = postRepository.findByTitleContainingIgnoreCase(title);
+    if(postsSearch!=null){
+        model.addAttribute("posts",postsSearch);
+        return "post";
+    }
+    return "redirect:/";
+    }
+
+    @PostMapping("/post/{id}/edit")
+    private String postEdit(@PathVariable(value = "id")Long id, @RequestParam String title, @RequestParam String text, Model model){
+        Post post = postRepository.findById(id).orElse(null);
+        if (post!=null){
+            if(!title.equals(post.getTitle()))
+                post.setTitle(title);
+            if(!text.equals(post.getContext()))
+                post.setContext(text);
+//            if(!img.getOriginalFilename().equals(post.getImageUrl())){
+//                try {
+//                    String filename = img.getOriginalFilename();
+//                    String filePath = "src/main/resources/static/" + filename;
+//                    try(var write = new FileOutputStream(filePath)) {
+//                        write.write(img.getBytes());
+//                    } catch (IOException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                    post.setImageUrl(filename);
+//                } catch (RuntimeException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            }
+            postRepository.save(post);
+        }
+        return "redirect:/";
+    }
+
+
 }
--- src/main/java/com/example/spring_cookingblog/Repositories/PostRepository.java
+++ src/main/java/com/example/spring_cookingblog/Repositories/PostRepository.java
@@ -3,5 +3,9 @@ package com.example.spring_cookingblog.Repositories;
 import com.example.spring_cookingblog.Models.Post;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PostRepository extends CrudRepository<Post,Long> {
+
+    List<Post> findByTitleContainingIgnoreCase(String title);
 }
